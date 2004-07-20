173dfc32add65233a426ce96508c124c04c67056
JESSE <jesse@cpan.org>
2004-07-20 17:10:00 +0000

    Import https://cpan.metacpan.org/authors/id/J/JE/JESSE/DBIx-SearchBuilder-1.02_02.tar.gz

--- inc/Module/Install.pm
+++ inc/Module/Install.pm
@@ -1,4 +1,4 @@
-#line 1 "inc/Module/Install.pm - /opt/perl-5.8.3/lib/site_perl/5.8.3/Module/Install.pm"
+#line 1 "inc/Module/Install.pm - /usr/local/share/perl/5.8.3/Module/Install.pm"
 # $File: //depot/cpan/Module-Install/lib/Module/Install.pm $ $Author: autrijus $
 # $Revision: #67 $ $Change: 1885 $ $DateTime: 2004/03/11 05:55:27 $ vim: expandtab shiftwidth=4
 
